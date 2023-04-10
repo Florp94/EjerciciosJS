679e10c69b9ab6c614536992e708eb895293da47
Florencia Pagella <flo.pagella94@hotmail.com>
2023-04-10 19:43:43 -0300

    ejercicios3

--- ejercicios.js
+++ ejercicios.js
@@ -13,18 +13,43 @@ else {
 
 // 2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.
 
+let numero1 = prompt ("Ingrese el primer número");
+let numero2 = prompt("Ingrese el segundo número");
 
+if (numero1>numero2){
+    console.log(numero1 + " Es mayor que " + numero2)
+};
+
+if (numero1<numero2) 
+    {console.log(numero1 + " Es menor que " + numero2)};
+
+    if (numero1 === numero2){
+    console.log("Son iguales");
+};       
+   
 
 // 3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5.
 
+let multiplos= 200
 
+if (multiplos % 5 === 0) {
+    console.log("El número " + multiplos + " es múltiplo de 5");
+}
+else {
+    console.log("El número ingresado no es múltiplo de 5");
+}
 
 // 4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.
 
 
-
 // 5 - Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado.
 
+function pasamelaRepe (palabra, repe) {
+    for (let i = 0; i < repe; i++) {
+        console.log(palabra);
+    }
+}
+    pasamelaRepe("Hola Guido", 3);
 
 
 // 6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
@@ -45,3 +70,5 @@ for (Multiplode10 of Numeros) {
 } 
 
 // 8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
+
+let NumeroArray = [1, 2, 3, 4, 5, 6, 7]
